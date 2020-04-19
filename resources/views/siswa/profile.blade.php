18dcd78424cb4ae8258edb30782ad60aac25bfdd
harisanwar129 <harisanwar129@gmail.com>
2020-04-19 11:44:54 +0700

    manambahka fitur relasi ke guru

--- resources/views/siswa/profile.blade.php
+++ resources/views/siswa/profile.blade.php
@@ -96,6 +96,7 @@
 												<th>Nama</th>
 												<th>Semester</th>
 												<th>Nilai</th>
+												<th>Guru</th>
 												<th>Aksi</th>
 											</tr>
 										</thead>
@@ -106,6 +107,7 @@
 												<td>{{$mapel->nama}}</td>
 												<td>{{$mapel->semester}}</td>
 												<td><a href="#" class="nilai" data-type="text" data-pk="{{$mapel->id}}" data-url="/api/siswa/{{$siswa->id}}/editnilai" data-title="Masukan Nilai">{{$mapel->pivot->nilai}}</a></td>
+												<td><a href="/guru/{{$mapel->guru_id}}/profile">{{$mapel->guru->nama}}</a></td>
 												<td><a href="/siswa/{{$siswa->id}}/{{$mapel->id}}/deletenilai" class="btn btn-danger btn-sm" onclick="return confirm ('apakah anda yakin akan menghapusnya?')">Hapus</a></td>
 											</tr>
 											@endforeach
